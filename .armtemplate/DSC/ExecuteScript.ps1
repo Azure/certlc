de3df0219f7e43ebcfb2d53622f9a5ca1482f415
Fabio Masciotra <fabmas@microsoft.com>
2023-11-27 13:35:54 +0100

    updated links to azure/certlc repository

--- .armtemplate/DSC/ExecuteScript.ps1
+++ .armtemplate/DSC/ExecuteScript.ps1
@@ -118,16 +118,16 @@ configuration ExecuteScript
             TestScript      = { return $false }
             SetScript       = {
 
-                # create a local folder named c:\temp\script and copy the "https://raw.githubusercontent.com/fabmas/certlc/main/.scripts/InstallEntRootCA.ps1" file into it
+                # create a local folder named c:\temp\script and copy the "https://raw.githubusercontent.com/azure/certlc/main/.scripts/InstallEntRootCA.ps1" file into it
                 $ScriptFolder="c:\temp\script"
                 New-Item -Path $ScriptFolder -ItemType Directory -Force |Out-Null
                 $ScriptName="InstallEntRootCA.ps1"
                 $ScriptPath="$ScriptFolder\$ScriptName"
-                $ScriptURL="https://raw.githubusercontent.com/fabmas/certlc/main/.scripts/InstallEntRootCA.ps1"
+                $ScriptURL="https://raw.githubusercontent.com/azure/certlc/main/.scripts/InstallEntRootCA.ps1"
                 Invoke-WebRequest -uri $ScriptURL -OutFile $ScriptPath
 
                 #Copy the utility to view the .eml file
-                Invoke-WebRequest -uri "https://raw.githubusercontent.com/fabmas/certlc/main/DemoTools/MailViewer.ps1" -OutFile "$ScriptFolder\Mailviewer.ps1"
+                Invoke-WebRequest -uri "https://raw.githubusercontent.com/azure/certlc/main/DemoTools/MailViewer.ps1" -OutFile "$ScriptFolder\Mailviewer.ps1"
 
                 #add link to default desktop
                 $desktopPath = 'C:\Users\Public\Desktop'
